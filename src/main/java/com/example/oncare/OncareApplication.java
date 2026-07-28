578bab695cf9982bc44be369e554c32228cab266
woghd9072 <woghd9072@gmail.com>
2026-07-28 12:39:38 +0900

    Initial commit

---
+++ src/main/java/com/example/oncare/OncareApplication.java
@@ -0,0 +1,13 @@
+package com.example.oncare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OncareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OncareApplication.class, args);
+	}
+
+}
